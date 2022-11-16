f78e9d0f640d75c463ab3acaadfce902d901fc8b
jkazuo55 <jkazuo555@gmail.com>
2022-11-16 12:26:37 -0400

    install capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.test.app',
+  appName: 'node12-angular10-capacitor4',
+  webDir: 'dist/node12-angular10-capacitor4',
+  bundledWebRuntime: false
+};
+
+export default config;
